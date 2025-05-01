5360d0db92bf0cfea9dd2f72703ab4a04de048ed
charlesaraya <charlesaraya@gmail.com>
2025-05-01 14:36:20 +0200

    refactor: remove unnecessary final url building

--- rag_techniques/simple_rag/prompter.py
+++ rag_techniques/simple_rag/prompter.py
@@ -3,9 +3,7 @@ import os
 from langchain_core.prompts import ChatPromptTemplate
 
 def build_prompt(path):
-    base_path = os.path.dirname(__file__)
-    prompt_path = os.path.join(base_path, path)
-    with open(prompt_path, "r") as f:
+    with open(path, "r") as f:
         prompt = f.read()
 
     prompt = ChatPromptTemplate.from_messages([
